d0309194afb42d6d45456e77503c0520c28db51a
froxworld <froxworld@gmail.com>
2019-05-19 11:10:43 +0200

    opencv

--- python/captureImage.py
+++ python/captureImage.py
@@ -3,5 +3,5 @@ from time import sleep
 
 camera = PiCamera()
 for i in range(5):
-	sleep(5)
-	camera.capture('/home/pi/francois/stage/python/image%s.jpg' % i)
+	sleep(1)
+	camera.capture('/home/pi/francois/stage/image%s.jpg' % i)
---
+++ python/captureVideo.py
@@ -0,0 +1,27 @@
+import time
+import picamera
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)
+    camera.start_preview()
+    camera.start_recording('home/pi/francois/stage/video/foo.h264')
+    time.sleep(0.2)
+    camera.stop_recording()
+    camera.stop_preview()
+
+
+
+
+
+
+#camera = picamera.PiCamera()
+#try:
+#    camera.start_preview()
+#    time.sleep(10)
+#    camera.stop_preview()
+#finally:
+#    camera.close()
+
+#camera.start_recording('/home/pi/francois/stage/video/video.avi')
+
