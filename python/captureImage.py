27975c0176b2ab0bc7a3ed9f0e7daf20c486bc80
froxworld <froxworld@gmail.com>
2019-05-19 10:21:08 +0200

    ras

--- python/captureImage.py
+++ python/captureImage.py
@@ -2,5 +2,6 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
-
-camera.capture('/home/pi/francois/stage/python/image1.jpg')
+for i in range(5):
+	sleep(5)
+	camera.capture('/home/pi/francois/stage/python/image%s.jpg' % i)
