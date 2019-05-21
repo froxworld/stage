d103231e9ece4c4ffbb59f55e4d75a10b1c5a0b7
francoisauxietre <Fafa1234!!>
2019-05-21 18:17:56 +0200

    retire de paho

--- mqtt_paho/client.py
+++ mqtt_paho/client.py
@@ -5,7 +5,8 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
+
+
     # reception de tous les messages
     #client.subscribe("$SYS/#")
     client.sucribe("test_channel")
