f547671611d3e960cc0cf4e904abf506bef5c021
francoisauxietre <Fafa1234!!>
2019-05-22 13:57:46 +0200

    modif client

--- mqtt_paho/client.py
+++ mqtt_paho/client.py
@@ -2,19 +2,21 @@ import paho.mqtt.client as mqtt
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    #
+    # print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
 
 
     # reception de tous les messages
     client.subscribe("$SYS/#")
-    #client.sucribe("test_channel")
+    client.sucribe("test_channel")
 
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
