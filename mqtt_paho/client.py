abc0171ce711bc5baebdf1c3a21869a6c6b0adec
francoisauxietre <Fafa1234!!>
2019-05-22 15:10:49 +0200

    e

--- mqtt_paho/client.py
+++ mqtt_paho/client.py
@@ -1,16 +1,36 @@
 import paho.mqtt.client as mqtt #import du client mqtt
 import time # pour affichier notre heure
 
+# methode de login au raapartiteur (client, userdata, level, buf)
+def on_log(client, donnee, niveau, tampon):
+    print("login:"+ tampon)
+
+#methode quand le client va se connecter
+def on_connect(client, donnee, drapeaux, resultat_de_connection):
+    if resultat_de_connection==0:
+        print("{0} connection reussie".format(client))
+    else:
+        print("probleme de connection code de retour =", resultat_de_connection)
+
 repartiteur = "10.42.0.1"  # adresse du repartiteur (broker)
 client = mqtt.Client("python1")  # creation de l'instance d un client
 print("connection au broker ", repartiteur)
 temps = time.clock()
 print (time.clock())
+
+client.on_connect = on_connect
+client.on_log = on_log
+
+
+
 client.connect(repartiteur)  # connection au repartiteur
 time.sleep(4)
 client.disconnect() # deconnection
 temps = temps - time.clock()
-print('le client %{0} est deconecter et est rester {1}'.format(client, temps))
+print('le client {0} est deconecter et est rester {1}'.format(client, temps))
+
+
+
 
 
 '''
