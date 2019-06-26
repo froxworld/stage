203d502dbf9f10f79a963481dbda1e2f44bac4cf
francoisauxietre <Fafa1234!!>
2019-06-26 16:19:12 +0200

    es

--- python/captureImage.py
+++ python/captureImage.py
@@ -2,22 +2,26 @@ from picamera import PiCamera
 from time import sleep
 import json  # pour sauvegarder  les coordonnees de chaque fichier
 
+import numpy as np
+import RPi.GPIO as gp
+import os
+import argparse
+import cv2
+
 numero = 0
 latitude = 0
 longitude = 0
 hauteur = 0
 vitesse_vent = 0
-roulis =0
+roulis = 0
 tangage = 0
 jour = 'lundi'
 annee = 2019
 mois = 'juin'
-heure =0
-minute =0
-nomFichierImage= 'raspberry{0}'.format(numero)
-
-
-# fichier_data = open('data.json', 'w')
+heure = 0
+minute = 0
+nomFichierImage = 'raspberry{0}'.format(numero)
+meteo = 'soleil' #soleil nuageux
 
 information = {
     'date': 'date',
@@ -34,15 +38,73 @@ information = {
     'heure': heure,
     'minute': minute,
     'nom_fichier_image': nomFichierImage
-
 }
 
+gp.setwarnings(False)  # pour acceder au cameras avec les pin 7 11 12
+gp.setmode(gp.BOARD)
+gp.setup(7, gp.OUT)
+gp.setup(11, gp.OUT)
+gp.setup(12, gp.OUT)
 camera = PiCamera()
-sleep(30)
 
-for i in range(1000):
-    sleep(1)
-    camera.capture('/home/pi/francois/stage/image/camera1-image%s.jpg' % i)
 
-with open('data.txt', 'w') as fichierjson:
-    json.dump(information, fichierjson)  # fermeture du fichier automatique avec with
+def main():
+    for index in range(3):
+        gp.output(7, False)
+        gp.output(11, False)
+        gp.output(12, True)
+        capture(1, index, meteo)
+
+        gp.output(7, True)
+        gp.output(11, False)
+        gp.output(12, True)
+        capture(2, index, meteo)
+
+        gp.output(7, False)
+        gp.output(11, True)
+        gp.output(12, False)
+        capture(3, index, meteo)
+
+        # sauvegarde des informations sous format json de toutes les photos, gps, ...
+        with open('data.txt', 'w') as fichierjson:
+            json.dump(information, fichierjson)  # fermeture du fichier automatique avec with
+
+
+def capture(numeroCamera, index, meteo):
+
+
+    # -awb sun cloud  pour deux type de temps pour prendre les photos
+
+
+    if numeroCamera == 1:
+        # reglages iso 200, awb balance des blancs auto, ex exposition auto , -a heure et date 20:09:33 10/12/2019
+        # -q qualite 100 , -o output chemin de sortie, -r raw fichier non compresser, -t time une photo toute les 2 secondes
+        # -e encoding jpg, -x exiff information dans la photo directement
+        cmd = 'raspistill -ISO 200 -awb auto -ex auto -a 12 -q 100 -t 2000 -o {0}{1}{2} -e jpg --exif WhiteBalance, GPSLatitude, GPSLongitude, GPSAltitude, GPSAreaInformation,'.format(
+            '/home/pi/francois/stage/image/camera', index, '.jpg')
+        camera.capture('/home/pi/francois/stage/image/camera1-image%s.jpg' % index)
+        os.system(cmd)
+
+    if numeroCamera == 2:
+        # reglages iso 200, awb balance des blancs auto, ex exposition auto , -a heure et date 20:09:33 10/12/2019
+        # -q qualite 100 , -o output chemin de sortie, -r raw fichier non compresser, -t time une photo toute les 2 secondes
+        # -e encoding jpg, -x exiff information dans la photo directement
+        cmd = 'raspistill -ISO 200 -awb auto -ex auto -a 12 -q 100 -t 2000 -o {0}{1}{2} -e jpg --exif WhiteBalance, GPSLatitude, GPSLongitude, GPSAltitude, GPSAreaInformation,'.format(
+            '/home/pi/francois/stage/image/camera', index, '.jpg')
+        camera.capture('/home/pi/francois/stage/image/camera2-image%s.jpg' % index)
+        os.system(cmd)
+
+    if numeroCamera == 3:
+        # reglages iso 200, awb balance des blancs auto, ex exposition auto , -a heure et date 20:09:33 10/12/2019
+        # -q qualite 100 , -o output chemin de sortie, -r raw fichier non compresser, -t time une photo toute les 2 secondes
+        # -e encoding jpg, -x exiff information dans la photo directement
+        cmd = 'raspistill -ISO 200 -awb auto -ex auto -a 12 -q 100 -t 2000 -o {0}{1}{2} -e jpg --exif WhiteBalance, GPSLatitude, GPSLongitude, GPSAltitude, GPSAreaInformation,'.format(
+            '/home/pi/francois/stage/image/camera', index, '.jpg')
+        camera.capture('/home/pi/francois/stage/image/camera3-image%s.jpg' % index)
+        os.system(cmd)
+
+
+if __name__ == "__main__":
+    main()
+
+# fichier_data = open('data.json', 'w')
--- take_pict.py
+++ take_pict.py
@@ -17,9 +17,6 @@ gp.setup(7, gp.OUT)
 gp.setup(11, gp.OUT)
 gp.setup(12, gp.OUT)
 
-gp.output(11, True)
-gp.output(12, True)
-
 
 def main():
     for i in range(2000):
@@ -38,24 +35,23 @@ def main():
         gp.output(12, False)
         capture(3)
 
-        image1 = cv2.imread(args['img1']
+        image1 = cv2.imread(args['img1']+ '{0}'.format(i))
         image2 = cv2.imread(args['img2'] + '{0}'.format(i))
         image3 = cv2.imread(args['img3'] + '{0}'.format(i))
 
 
 def capture(cam):
     if cam == 1:
-        cmd = "raspistill -vf -hf -t 1000 -o %s" % args['img1']+ '{0}'.format(i))
-    if cam == 2:
-        cmd = "raspistill -vf -hf -t 1000 -o %s" % args['img2']+ '{0}'.format(i))
-    if cam == 3:
-        cmd = "raspistill -vf -hf -t 1000 -o %s" % args['img3']+ '{0}'.format(i))
-    os.system(cmd)
+        cmd = "raspistill -vf -hf -t 1000 -o %s" % args['img1'] + '{0}'.format(i)
+        if cam == 2:
+            cmd = "raspistill -vf -hf -t 1000 -o %s" % args['img2'] + '{0}'.format(i)
+        if cam == 3:
+            cmd = "raspistill -vf -hf -t 1000 -o %s" % args['img3'] + '{0}'.format(i)
+        os.system(cmd)
 
+        if __name__ == "__main__":
+            main()
 
-if __name__ == "__main__":
-    main()
-
-    gp.output(7, False)
-    gp.output(11, False)
-    gp.output(12, True)
+        gp.output(7, False)
+        gp.output(11, False)
+        gp.output(12, True)
